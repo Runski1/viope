513db4074b65ad10c41e1c0b2ad1bde7a1baa29d
Matias Ruonala <142801440+Runski1@users.noreply.github.com>
2023-09-23 23:02:04 +0300

    excercise2 done Test-environment playground.py created

--- Viope Python AI course/exercise2.py
+++ Viope Python AI course/exercise2.py
@@ -8,8 +8,11 @@
 # the user selects 0, the first item is removed. When the user quits, the final list is printed for the user "
 # The following items remain in the list:" followed by the remaining items one per line. If the user selects anything
 # outside the options, including when deleting items, the program responds "Incorrect selection.". When the program
-# works correctly it prints out the following:
-grocery_list =[]
+# works correctly, it prints out the following:
+
+grocery_list = []
+
+
 def error_print():
     print("Incorrect selection.")
 
@@ -17,24 +20,51 @@ def error_print():
 def add_items():
     grocery = input("What will be added?: ")
     grocery_list.append(grocery)
-# MIGHT NEED TO USE grocery_list AS PARAMETER
+
+
+def remove_items():
+    str1 = "There are "
+    str2 = " items in the list."
+    print(str1 + str(len(grocery_list)) + str2)
+    removable = input("Which item is deleted?: ")
+    try:
+        grocery_list.pop(int(removable))
+    except (ValueError, IndexError):
+        error_print()
+
+
+def quit_code():
+    print("The following items remain in the list:")
+    for item in range(len(grocery_list)):
+        print(grocery_list[item])
+    quit()
+
+
 while True:
     user_input = input("Would you like to\n"
                        "(1)Add or\n"
                        "(2)Remove items or\n"
                        "(3)Quit?: ")
-    match user_input:
-        case "1":
-            add_items()
-        case "2":
-            remove_items()
-        case "3":
-            for item in range(len(grocery_list)):
-                print(grocery_list[item])
-            quit()
-        case _:
-            error_print()
+# Match case is for python 3.10 only
+# Use dictionary or elif monster instead
+#     match user_input:
+#         case "1":
+#             add_items()
+#         case "2":
+#             remove_items()
+#         case "3":
+#             quit_code()
+#         case _:
+#             error_print()
 
-
-
-
+# Here's the code for dictionary
+    input_options = {
+        "1": add_items,
+        "2": remove_items,
+        "3": quit_code,
+    }
+    selected_option = input_options.get(user_input)
+    if selected_option:
+        selected_option()
+    else:
+        error_print()
---
+++ Viope Python AI course/playground.py
@@ -0,0 +1,2 @@
+fruits = ["apple", "banana", "Cherry"]
+fruits.pop(int(input("Monesko poistetaan?")))
